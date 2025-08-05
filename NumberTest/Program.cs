684d60edf51c35e7a32e73512ff777c68a426717
Joshua David Jones <the8bitbass@gmail.com>
2025-08-05 15:43:28 +1000

    Change Program.cs

--- NumberTest/Program.cs
+++ NumberTest/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class NumberTest {
+    public static int Main(string[] args) {
+        return 0;
+    }
+}
